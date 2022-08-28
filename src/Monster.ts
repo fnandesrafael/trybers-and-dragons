da537cd92edc0614c849f29543ba3a8125cc086c
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-08-28 09:46:13 -0300

    feat: requisito 09 - implementação da classe Monster e refatoração da classe Character

---
+++ src/Monster.ts
@@ -0,0 +1,31 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifepoints: number;
+  private _strength: number;
+  
+  constructor() {
+    this._lifepoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifepoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints > 0) {
+      this._lifepoints -= attackPoints;
+    } if (this._lifepoints < 0) {
+      this._lifepoints = -1;
+    } return this._lifepoints;
+  }
+
+  attack(enemy: Fighter | SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
