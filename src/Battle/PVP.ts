1f749c7597a76ac7322d48cea70427c75f00f88b
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-08-28 10:09:15 -0300

    feat: requisito 10 - implementação da classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,17 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(private player1: Fighter, private player2: Fighter) {
+    super(player1);
+  }
+
+  fight(): number {
+    while (this.player1.lifePoints >= 0 && this.player2.lifePoints >= 0) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+
+    return super.fight();
+  }
+}
