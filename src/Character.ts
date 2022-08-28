d305f766235c1902a0a76595e77951cb7af9880b
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-08-28 09:12:18 -0300

    feat: requisito 07 - implementação da classe Character

---
+++ src/Character.ts
@@ -0,0 +1,100 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+export default class Character implements Fighter {
+  private _name: string;
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._name = name;
+    /**
+     * Referência usada para a geração de valor aleatório, retirada da própria documentação do JS:
+     * https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random#gerando_um_n%C3%BAmero_inteiro_aleat%C3%B3rio_entre_dois_valores
+     */
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: 'mana',
+      amount: getRandomInt(1, 10),
+    };
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy {
+    return { type_: this._energy.type_, amount: this._energy.amount };
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const reducedDamage = attackPoints - this._defense;
+    if (reducedDamage > 0) {
+      this._lifePoints -= reducedDamage;
+    } if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    const increment = getRandomInt(1, 10);
+
+    this._maxLifePoints += increment;
+    this._strength += increment;
+    this._dexterity += increment;
+    this._defense += increment;
+    this._lifePoints = this._maxLifePoints;
+    this._energy.amount = 10;
+
+    if (this._maxLifePoints > this.race.maxLifePoints) {
+      this._maxLifePoints = this.race.maxLifePoints;
+    }
+    if (this._lifePoints > this._maxLifePoints) {
+      this._lifePoints = this._maxLifePoints;
+    }
+  }
+}
